c79888ac29d0a17581ea681478a713a5e2d71489
jacques <jacques.bosch@inversion.dev>
2022-01-01 13:58:33 +0200

    init repo

---
+++ Mutation/MutationForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Mutation
+{
+	partial class MutationForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblActiveMic = new System.Windows.Forms.Label();
+			this.lbl2 = new System.Windows.Forms.Label();
+			this.txtActiveMic = new System.Windows.Forms.TextBox();
+			this.txtAllMics = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// lblActiveMic
+			// 
+			this.lblActiveMic.AutoSize = true;
+			this.lblActiveMic.Location = new System.Drawing.Point(137, 13);
+			this.lblActiveMic.Name = "lblActiveMic";
+			this.lblActiveMic.Size = new System.Drawing.Size(57, 13);
+			this.lblActiveMic.TabIndex = 0;
+			this.lblActiveMic.Text = "Active Mic";
+			// 
+			// lbl2
+			// 
+			this.lbl2.AutoSize = true;
+			this.lbl2.Location = new System.Drawing.Point(15, 41);
+			this.lbl2.Name = "lbl2";
+			this.lbl2.Size = new System.Drawing.Size(16, 13);
+			this.lbl2.TabIndex = 1;
+			this.lbl2.Text = "...";
+			// 
+			// txtActiveMic
+			// 
+			this.txtActiveMic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtActiveMic.Location = new System.Drawing.Point(216, 10);
+			this.txtActiveMic.Name = "txtActiveMic";
+			this.txtActiveMic.ReadOnly = true;
+			this.txtActiveMic.Size = new System.Drawing.Size(257, 20);
+			this.txtActiveMic.TabIndex = 2;
+			// 
+			// txtAllMics
+			// 
+			this.txtAllMics.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtAllMics.Location = new System.Drawing.Point(216, 36);
+			this.txtAllMics.Multiline = true;
+			this.txtAllMics.Name = "txtAllMics";
+			this.txtAllMics.ReadOnly = true;
+			this.txtAllMics.Size = new System.Drawing.Size(257, 58);
+			this.txtAllMics.TabIndex = 3;
+			// 
+			// MutationForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(477, 100);
+			this.Controls.Add(this.txtAllMics);
+			this.Controls.Add(this.txtActiveMic);
+			this.Controls.Add(this.lbl2);
+			this.Controls.Add(this.lblActiveMic);
+			this.Name = "MutationForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Mutation";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MutationForm_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblActiveMic;
+		private System.Windows.Forms.Label lbl2;
+		private System.Windows.Forms.TextBox txtActiveMic;
+		private System.Windows.Forms.TextBox txtAllMics;
+	}
+}
+
---
+++ Mutation/MutationForm.cs
@@ -0,0 +1,124 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace Mutation
+{
+	public partial class MutationForm : Form
+	{
+		internal Hotkey _hkToggleMicMute;
+		private bool IsMuted = false;
+		private CoreAudioController _audioController;
+		IEnumerable<CoreAudioDevice> _devices;
+		private CoreAudioDevice Microphone { get; set; }
+
+		public MutationForm()
+		{
+			InitializeComponent();
+			InitializeAudioControls();
+			HookupHotkeys();
+		}
+
+		internal void InitializeAudioControls()
+		{
+			txtActiveMic.Text = "(Initializing...)";
+			Application.DoEvents();
+
+			_audioController = new CoreAudioController();
+			_devices = _audioController.GetDevices(DeviceType.Capture, DeviceState.Active);
+			var defaultMicDevice = _devices
+				.FirstOrDefault(x => x.IsDefaultDevice);
+			if (defaultMicDevice != null)
+			{
+				this.Microphone = defaultMicDevice;
+
+				FeedbackToUser();
+			}
+			else
+			{
+				txtActiveMic.Text = "(Unable to find device)";
+				Console.Beep(300, 100);
+				Console.Beep(300, 100);
+				Console.Beep(300, 100);
+			}
+		}
+
+		public void ToggleMicrophoneMute()
+		{
+			lock (this)
+			{
+				IsMuted = !IsMuted;
+				foreach (var mic in _devices)
+					mic.Mute(IsMuted);
+
+				FeedbackToUser();
+			}
+		}
+
+		private void FeedbackToUser()
+		{
+			lock (this)
+			{
+				if (Microphone.IsMuted)
+				{
+					this.Text = "Muted Microphone";
+					this.BackColor = Color.LightGray;
+					Console.Beep(500, 200);
+				}
+				else
+				{
+					this.Text = "Unuted Microphone";
+					this.BackColor = Color.White;
+					Console.Beep(1300, 50);
+				}
+
+				txtActiveMic.Text = this.Microphone.Name;
+
+				int i = 1;
+				txtAllMics.Text = string.Join(Environment.NewLine, _devices.Select(m => $"{i++}) {m.FullName}{(m.IsMuted ? "       - muted" : "")}").ToArray());
+
+			}
+		}
+
+		private void HookupHotkeys()
+		{
+			HookupToggleMichrophoneMuteHotkey();
+			HookupPushToUnmuteMichriphoneHotkey();
+			lbl2.Text = "Toggle Michrophone Mute: " + _hkToggleMicMute;
+		}
+
+		private void HookupToggleMichrophoneMuteHotkey()
+		{
+			_hkToggleMicMute = new Hotkey();
+			_hkToggleMicMute.Control = true;
+			_hkToggleMicMute.Shift = true;
+			_hkToggleMicMute.KeyCode = Keys.Z;
+			_hkToggleMicMute.Pressed += delegate { ToggleMicrophoneMute(); };
+
+			TryRegisterHotkey(_hkToggleMicMute);
+		}
+
+		private void HookupPushToUnmuteMichriphoneHotkey()
+		{
+			_hkToggleMicMute = new Hotkey();
+			_hkToggleMicMute.Alt = true;
+			_hkToggleMicMute.KeyCode = Keys.Q;
+			_hkToggleMicMute.Pressed += delegate { ToggleMicrophoneMute(); };
+
+			TryRegisterHotkey(_hkToggleMicMute);
+		}
+
+		private void TryRegisterHotkey(Hotkey hotkey)
+		{
+			if (!hotkey.GetCanRegister(this))
+				MessageBox.Show("Whoops, looks like attempts to register the hotkey " + hotkey + " will fail or throw an exception.");
+			else
+				hotkey.Register(this);
+		}
+
+		private void MutationForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (_hkToggleMicMute != null && _hkToggleMicMute.Registered)
+				_hkToggleMicMute.Unregister();
+		}
+	}
+}
