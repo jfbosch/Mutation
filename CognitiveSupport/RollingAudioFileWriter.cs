2d5d6b5140294e5024162eac90597985061f7720
jacques <jacques.bosch@inversion.dev>
2023-09-14 10:44:59 +0200

    switch to recording to wave

--- CognitiveSupport/RollingAudioFileWriter.cs
+++ CognitiveSupport/RollingAudioFileWriter.cs
@@ -1,95 +1,122 @@
 ﻿using NAudio.Lame;
 using NAudio.Wave;
 using StringExtensionLibrary;
+using System;
+using System.IO;
 
-namespace CognitiveSupport
+namespace CognitiveSupport;
+
+public class RollingAudioFileWriter : IDisposable
 {
-	public class RollingAudioFileWriter : IDisposable
+	private readonly string _tempPrefix = "temp_";
+
+	private readonly object _lock = new object();
+	private DirectoryInfo _sessionDirectory;
+
+	private volatile WaveFileWriter _activeWaveWriter;
+
+	private int _fileIndex = 0;
+	private DateTime _lastFileSwitch = DateTime.UtcNow;
+
+	public RollingAudioFileWriter(DirectoryInfo outputDirectory)
 	{
-		private readonly string _tempPrefix = "temp_";
+		_sessionDirectory = outputDirectory ?? throw new ArgumentNullException(nameof(outputDirectory));
+		string session = $"Session_{DateTime.UtcNow:yyyy-MM-dd_HH-mm-ss}";
+		_sessionDirectory = new DirectoryInfo(Path.Combine(_sessionDirectory.FullName, session));
+		if (!_sessionDirectory.Exists)
+			_sessionDirectory.Create();
+	}
 
-		private readonly object _lock = new();
-		private DirectoryInfo _sessionDirectory;
+	public void Write(byte[] buffer, int offset, int count, WaveFormat waveFormat)
+	{
+		if (buffer is null)
+			throw new ArgumentNullException(nameof(buffer));
 
-		private volatile LameMP3FileWriter _activeMp3Writer;
-
-		private int _fileIndex = 0;
-		private DateTime _lastFileSwitch = DateTime.UtcNow;
-
-		public RollingAudioFileWriter(
-			DirectoryInfo outputDirectory)
+		lock (_lock)
 		{
-			_sessionDirectory = outputDirectory ?? throw new ArgumentNullException(nameof(outputDirectory));
-			string session = $"Session_{DateTime.UtcNow:yyyy-MM-dd_HH-mm-ss}";
-			_sessionDirectory = new DirectoryInfo(Path.Combine(_sessionDirectory.FullName, session));
-			if (!_sessionDirectory.Exists)
-				_sessionDirectory.Create();
-		}
+			bool timeToSwitchFiles = false;
+			if (_fileIndex <= 0)
+				timeToSwitchFiles = true;
+			var elapsedFileDuration = DateTime.UtcNow - _lastFileSwitch;
+			if (elapsedFileDuration >= TimeSpan.FromSeconds(1))
+				timeToSwitchFiles = true;
 
-		public void Write(
-			byte[] buffer,
-			int offset,
-			int count,
-			WaveFormat waveFormat)
-		{
-			if (buffer is null)
-				throw new ArgumentNullException(nameof(buffer));
-
-			lock (_lock)
+			if (timeToSwitchFiles)
 			{
-				bool timeToSwitchFiles = false;
-				if (_fileIndex <= 0)
-					timeToSwitchFiles = true;
-				var elapsedFileDuration = DateTime.UtcNow - _lastFileSwitch;
-				if (elapsedFileDuration >= TimeSpan.FromSeconds(1))
-					timeToSwitchFiles = true;
+				_fileIndex++;
+				_lastFileSwitch = DateTime.UtcNow;
+				_activeWaveWriter?.Dispose();
 
-				if (timeToSwitchFiles)
+				int previousFileIndex = _fileIndex - 1;
+				RenameTempFileToFinalClipFile(previousFileIndex);
+
+				string tempFileName = GetTempIndexFileName(_fileIndex);
+				string tempFilePath = Path.Combine(_sessionDirectory.FullName, tempFileName);
+
+				_activeWaveWriter = new WaveFileWriter(tempFilePath, waveFormat);
+				timeToSwitchFiles = false;
+			}
+
+			_activeWaveWriter.Write(buffer, offset, count);
+		}
+	}
+
+	private string GetTempIndexFileName(int index)
+	{
+		return $"{_tempPrefix}clip_{index:0000}.wav";
+	}
+
+	private void RenameTempFileToFinalClipFile(int fileIndexToRename)
+	{
+		string tempFileName = GetTempIndexFileName(fileIndexToRename);
+		string tempFilePath = Path.Combine(_sessionDirectory.FullName, tempFileName);
+		if (File.Exists(tempFilePath))
+		{
+			string finalFileName = tempFileName.RemovePrefix(_tempPrefix);
+			string finalFilePath = Path.Combine(_sessionDirectory.FullName, finalFileName);
+			File.Move(tempFilePath, finalFilePath);
+		}
+	}
+
+	public void Dispose()
+	{
+		lock (_lock)
+		{
+			_activeWaveWriter?.Dispose();
+			_activeWaveWriter = null;
+			RenameTempFileToFinalClipFile(_fileIndex);
+			SpliceClips();
+		}
+	}
+
+
+	public void SpliceClips()
+	{
+		string outputPath = Path.Combine(_sessionDirectory.FullName, "_output.mp3");
+
+		var waveFiles = _sessionDirectory.GetFiles("clip_*.wav").OrderBy(f => f.Name).ToList();
+		if (waveFiles.Count == 0)
+			return;
+
+		using (var reader = new WaveFileReader(waveFiles[0].FullName))
+		using (var mp3Writer = new LameMP3FileWriter(outputPath, reader.WaveFormat, LAMEPreset.STANDARD))
+		{
+			byte[] buffer = new byte[reader.WaveFormat.AverageBytesPerSecond * 4];
+
+			foreach (var waveFile in waveFiles)
+			{
+				using (var waveReader = new WaveFileReader(waveFile.FullName))
 				{
-					_fileIndex++;
-					_lastFileSwitch = DateTime.UtcNow;
-					_activeMp3Writer?.Dispose();
-
-					int previousFileIndex = _fileIndex - 1;
-					RenameTempFileToFinalClipFile(previousFileIndex);
-
-					string tempFileName = GetTempIndexFileName(_fileIndex);
-					string tempFilePath = Path.Combine(_sessionDirectory.FullName, tempFileName);
-
-					_activeMp3Writer = new LameMP3FileWriter(tempFilePath, waveFormat, LAMEPreset.STANDARD);
-					timeToSwitchFiles = false;
+					int read;
+					while ((read = waveReader.Read(buffer, 0, buffer.Length)) > 0)
+					{
+						mp3Writer.Write(buffer, 0, read);
+					}
 				}
-
-				_activeMp3Writer.Write(buffer, offset, count);
-			}
-		}
-
-		private string GetTempIndexFileName(int index)
-		{
-			return $"{_tempPrefix}clip_{index:0000}.mp3";
-		}
-
-		private void RenameTempFileToFinalClipFile(
-			int fileIndexToRename)
-		{
-			string tempFileName = GetTempIndexFileName(fileIndexToRename);
-			string tempFilePath = Path.Combine(_sessionDirectory.FullName, tempFileName);
-			if (File.Exists(tempFilePath))
-			{
-				string finalFileName = tempFileName.RemovePrefix(_tempPrefix);
-				string finalFilePath = Path.Combine(_sessionDirectory.FullName, finalFileName);
-				File.Move(tempFilePath, finalFilePath);
-			}
-		}
-
-		public void Dispose()
-		{
-			lock (_lock)
-			{
-				_activeMp3Writer?.Dispose();
-				_activeMp3Writer = null;
-				RenameTempFileToFinalClipFile(_fileIndex);
 			}
 		}
 	}
+
 }
+
+
